91e5c52433996eabaf0b4536169cd1be89900f51
Paranoid Marvin <paranoidmarvin0@gmail.com>
2018-11-01 16:53:27 -0600

    init assignment 4

---
+++ Lecture/assignment_four/polynomial.h
@@ -0,0 +1,104 @@
+#ifndef __POLYNOMIAL_H___
+#define __POLYNOMIAL_H___
+
+// Domain
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// evaluate
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// add
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// subtract
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// multiply
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// multiply
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// raiseDegree
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// equal 
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// getDegree
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// getCoefficient 
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+// Print 
+//
+// Purpose: 
+// Parameter(s): N/A
+// Precondition: N/A
+// Returns: 
+// Side Effect: N/A
+//
+
+
+#endif
