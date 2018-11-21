27e85a700b8d379fb4883de5a8ff8e4148aa4b52
Paranoid Marvin <paranoidmarvin0@gmail.com>
2018-11-21 17:35:02 -0600

    final edits

--- Lecture/assignment_five/priorityQueue.cpp
+++ Lecture/assignment_five/priorityQueue.cpp
@@ -10,14 +10,19 @@ priorityQueue::priorityQueue(){
 };
 
 void priorityQueue::insert(int x){
-
+if (pQArr.size()==0)
+{
+    pQArr.insert(pQArr.size(), x);
+}
+else{
 for (unsigned int i = 0; i < pQArr.size(); i++)
 if (pQArr.read(i) >= x)
 {pQArr.insert(i, x);
 break;
 }
+else{pQArr.insert(pQArr.size(),x);}
 
-
+}
 };
 
 int priorityQueue::remove(){
