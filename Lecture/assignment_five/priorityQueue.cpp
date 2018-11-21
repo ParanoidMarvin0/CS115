8dbaa66411e54dc1f7ede5e4e3ae078b7917fbf9
Paranoid Marvin <paranoidmarvin0@gmail.com>
2018-11-20 18:06:42 -0600

    init assignment 5

---
+++ Lecture/assignment_five/priorityQueue.cpp
@@ -0,0 +1,4 @@
+#ifndef __PRIORITYQUEUE_H__
+#define __PRIORITYQUEUE_H__
+
+#endif
---
+++ Lecture/assignment_five/queue.h
@@ -0,0 +1,12 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+#include <managed_array.h>
+#include <container.h>
+
+class Queue : public Container {
+
+}
+
+
+#endif
+
